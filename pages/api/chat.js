25b2fed9261fa6e146589bc66b7731592a5d579b
Raw Scripter <37735121+rawscripter@users.noreply.github.com>
2023-03-28 06:22:27 +0600

    cleanup

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,33 +1,9 @@
-// import { Configuration, OpenAIApi } from 'openai'
-import { ChatGPTAPI } from 'chatgpt'
+ import { ChatGPTAPI } from 'chatgpt'
 
 export default async function handler(req, res) {
     //  check if the request is a POST request
     if (req.method === 'POST') {
         const { message, id } = req.body;
-
-        // res.json({
-        //     id: "chatcmpl-6yqKAYvCyAN3XlVWGsKeuR8DzCAC9",
-        //     text: "As an AI language model, I don't have feelings, but I'm functioning well. How can I assist you today?",
-        //     role: "bot",
-        //     question: message
-        // });
-
-        // const configuration = new Configuration({
-        //     apiKey: "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",
-        // });
-        // const openai = new OpenAIApi(configuration);
-        // openai.createCompletion({
-        //     model: "text-davinci-002",
-        //     prompt: message,
-        // }).then((completion) => {
-        //     res.json({
-        //         id: completion.data.id,
-        //         text: completion.data.choices[0].text,
-        //     });
-        // }).catch((error) => {
-        //     res.json(error);
-        // });
 
         const api = new ChatGPTAPI({
             apiKey: process.env.CHATGPT
